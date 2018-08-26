23b6abebecc7e383ad88f94774ca7c56c6134d58
Tim Poerschke <info@coder-tim.de>
2018-08-26 15:27:19 +0200

    [FIX] Post List is now used in index file.

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
 <?php get_header(); ?>
 
-	<section class="blogpost-list">
-		<?php while (have_posts()) : the_post(); ?>
-			<article class="blogpost">
-				<h2 class="title"> <a href="<?php the_permalink() ?>"><?php the_title(); ?> </a> </h2>
-				<?php if (has_post_thumbnail()) : ?>
-					<img class='thumbnail' src="<?php the_post_thumbnail_url('full'); ?>" alt="<?php the_title(); ?>"/>
-				<?php endif; ?>
-				<?php
-					the_content('weiterlesen &raquo;','','');
-				?>
-				<div class="blogpost-meta">
-						<?php the_category(', '); ?> &lowast; <?php the_time('j. F Y') ?>
-				</div>
-			</article> <!-- article -->
-		<?php endwhile; ?>
-		<?php show_pagination(); ?>
-	</section>
+	<?php get_template_part('template-parts/post/list'); ?>
 
 <?php get_footer(); ?>
